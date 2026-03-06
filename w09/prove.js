cb33b27d2dd5540958beecf6f6308768d3e68c7d
kelseyschae <kelsschaefermeyer2004@gmail.com>
2026-03-06 16:13:27 -0700

    edited

---
+++ w09/prove.js
@@ -0,0 +1,9 @@
+function toggleNav() {
+
+  var icon = document.getElementById("menu-icon");
+  var sidebar = document.getElementById("sidebar");
+
+  icon.classList.toggle("open");
+  sidebar.classList.toggle("open");
+
+}
